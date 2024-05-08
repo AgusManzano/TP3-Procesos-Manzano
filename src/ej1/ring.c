fe25bf9f1c814fb1504db5ddd19a3f8cf104d8db
AgusManzano <amanzano@udesa.edu.ar>
2024-05-07 22:18:12 -0300

    final

--- src/ej1/ring.c
+++ src/ej1/ring.c
@@ -4,17 +4,84 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
 int main(int argc, char **argv)
 {	
-	int start, status, pid, n;
+	int pid, n;
 	int buffer[1];
 
+
 	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
-    
-    /* Parsing of arguments */
-  	/* TO COMPLETE */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
-    
-   	/* You should start programming from here... */
+	
+
+
+	/* Parsing of arguments */
+	n = atoi(argv[1]);
+	int c = atoi(argv[2]);
+	int s = atoi(argv[3]);
+	
+	printf("n: %i, c: %i, s: %i\n", n, c, s);
+
+	printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, c, s);	
+	
+	int pipes[n][2]; // Array of pipes
+	
+	// Create pipes
+	for (int i = 0; i < n; i++) {
+		if (pipe(pipes[i]) == -1) {
+			perror("Error creating pipe");
+			exit(1);
+		}
+	}
+	
+	// Create child processes
+	for (int i = 0; i < n; i++) 
+	{
+    pid = fork();
+
+    if (pid < 0) {
+        perror("Error creating child process");
+        exit(1);
+
+    } 
+	else if (pid == 0) 
+	{
+		// Child process
+		close(pipes[i][1]); // Close write end of the pipe
+
+		if (i == s) 
+		{
+			// This is the process that will send the character
+			buffer[0] = c;
+			printf("Proceso %i enviará el caracter %i al siguiente proceso\n", i, buffer[0]);
+			buffer[0]++;
+		} 
+		else 
+		{
+			// Other processes
+			while (read(pipes[i][0], buffer, sizeof(buffer)) == 0) 
+			{
+				// Wait for something to be written to the pipe
+			}
+			printf("Proceso %i recibió el caracter %i y lo enviará al siguiente proceso\n", i, buffer[0]);
+			buffer[0]++;
+		}
+
+		close(pipes[i][0]); // Close read end of the pipe
+		write(pipes[(i + 1) % n][1], buffer, sizeof(buffer)); // Write to next process
+		close(pipes[(i + 1) % n][1]); // Close write end of the pipe
+
+		// Close the read and write ends of all pipes
+		for (int j = 0; j < n; j++) 
+		{
+			close(pipes[j][0]);
+			close(pipes[j][1]);
+		}
+
+		exit(0);
+	}
+	}
+	return 0;
 }
+
+
+
--- src/ej2/shell.c
+++ src/ej2/shell.c
@@ -3,8 +3,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_COMMANDS 200
+
+char* handleQuotes(char* arg);
 
 int main() {
 
@@ -16,7 +19,7 @@ int main() {
     {
         printf("Shell> ");
         
-        /*Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
+        /* Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
         fgets(command, sizeof(command), stdin);
         
         /* Removes the newline character (\n) from the end of the string stored in command, if present. 
@@ -38,10 +41,129 @@ int main() {
         }
 
         /* You should start programming from here... */
+
+        // Create an array of pipes to connect the commands
+        int pipes[command_count - 1][2];
         for (int i = 0; i < command_count; i++) 
         {
-            printf("Command %d: %s\n", i, commands[i]);
-        }    
+            // Initialize the pipes
+            if (i < command_count - 1) 
+            {
+                if (pipe(pipes[i]) == -1) 
+                {
+                    perror("pipe");
+                    return 1;
+                }
+            }
+
+            pid_t pid = fork();
+            if (pid == -1) 
+            {
+                perror("fork");
+                return 1;
+            } 
+            else if (pid == 0) 
+            {
+                // Child process
+
+                // Redirect the input of the child process to the previous pipe
+                if (i > 0) 
+                {
+                    if (dup2(pipes[i-1][0], STDIN_FILENO) == -1) 
+                    {
+                        perror("dup2");
+                        return 1;
+                    }
+                }
+
+                // Redirect the output of the child process to the current pipe
+                if (i < command_count - 1) 
+                {
+                    if (dup2(pipes[i][1], STDOUT_FILENO) == -1) 
+                    {
+                        perror("dup2");
+                        return 1;
+                    }
+                }
+
+                // Close unused file descriptors
+                for (int j = 0; j < command_count - 1; j++) 
+                {
+                    close(pipes[j][0]);
+                    close(pipes[j][1]);
+                }
+
+                // Execute the corresponding program
+                char *args[256];
+                int arg_count = 0;
+                char *arg = strtok(commands[i], " ");
+                while (arg != NULL) 
+                {
+                    args[arg_count++] = handleQuotes(arg);
+                    arg = strtok(NULL, " "); 
+
+                
+                }
+                // printf("Executing command: %s\n", args[0]); // Print the command being executed
+
+                // printf("Output:\n"); // Print the output of the command
+                // printf("args[0]: %s\n", args[0]);
+
+                // for (int j = 0; j < arg_count; j++) 
+                // {
+                //     printf("args[%d]: %s\n", j, args[j]); // Print the arguments of the command
+                // }
+                
+                // add NULL to the end of the arguments array
+                args[arg_count] = NULL;
+
+                execvp(args[0], args);
+                perror("execvp");
+                return 1;
+            } 
+            else 
+            {
+                // Parent process
+                
+                // Close the read file descriptor of the previous pipe
+                if (i > 0) 
+                {
+                    close(pipes[i-1][0]);
+                }
+                // Close the write file descriptor of the current pipe
+                if (i < command_count - 1) 
+                {
+                    close(pipes[i][1]);
+                }
+            }
+        }
+
+        // Wait for all child processes to finish
+        for (int i = 0; i < command_count; i++) 
+        {
+            wait(NULL);
+        }
+
+        // Reset the command count
+        command_count = 0;
     }
     return 0;
+}
+
+
+char* handleQuotes(char* arg) 
+{
+    // If the argument starts with a quote, find the matching quote
+    if (arg[0] == '"') 
+    {
+        // Remove the starting quote
+        arg++;
+        // Find the ending quote and remove it
+        char *end_quote = strchr(arg, '"');
+        if (end_quote != NULL) 
+        {
+            *end_quote = '\0';
+        }
+    }
+    return arg;
 }
